12e3623423f870925646708576540cd5a6952cd2
pudgecon <pudgecon@gmail.com>
2012-09-02 10:49:02 +0800

    Latest build.

--- assets/javascripts/search.js
+++ assets/javascripts/search.js
@@ -1,10 +1,27 @@
+//function goSearch() {
+    //if($('#search-text').val().trim() == '') {
+      //return false;
+    //} else {
+      //$('#search-btn').attr('href', 'http://www.google.com/search?q=' + $('#search-text').val() + '%20site:www.xn--7dvr7nd0f.cn');
+      //return true;
+    //}
+//}
+
 $(function() {
   $('#search-btn').click(function() {
+    //return goSearch();
     if($('#search-text').val().trim() == '') {
       return false;
     } else {
       $(this).attr('href', 'http://www.google.com/search?q=' + $('#search-text').val() + '%20site:www.xn--7dvr7nd0f.cn');
       return true;
+    }
+  });
+  $('#search-text').keydown(function(e){
+    if(e.keyCode == 13) {
+      //return goSearch();
+      //$('#search-btn').trigger('click');
+      $('#search-btn').click();
     }
   });
 });
